5042c2232af5cb035fe85b90e2f9687fbd38d622
Denis Maximenko <manlok71@gmail.com>
2023-01-07 19:29:08 +0400

    Add some constructors

--- Source/LD52/InvasionGameMode.cpp
+++ Source/LD52/InvasionGameMode.cpp
@@ -3,3 +3,8 @@
 
 #include "InvasionGameMode.h"
 
+
+AInvasionGameMode::AInvasionGameMode()
+{
+	
+}
--- Source/LD52/InvasionPlayerState.cpp
+++ Source/LD52/InvasionPlayerState.cpp
@@ -3,3 +3,8 @@
 
 #include "InvasionPlayerState.h"
 
+
+AInvasionPlayerState::AInvasionPlayerState()
+{
+
+}
