a849f1ef3fe993aab919c9e5f6cff7424ec88fa2
Denis Maximenko <manlok71@gmail.com>
2023-01-07 21:38:23 +0400

    Codestyle fixes

--- Source/LD52/Private/Planet/SphericalMovement.cpp
+++ Source/LD52/Private/Planet/SphericalMovement.cpp
@@ -9,7 +9,6 @@ USphericalMovement::USphericalMovement()
 	PrimaryComponentTick.bCanEverTick = true;
 	IsMoving = false;
 	MinimalDistance = 1;
-	
 }
 
 void USphericalMovement::TickComponent(float DeltaTime, ELevelTick TickType,
@@ -20,14 +19,13 @@ void USphericalMovement::TickComponent(float DeltaTime, ELevelTick TickType,
 	if (IsMoving)
 	{
 		const FVector2D CurrentLocation = OwnerSpherePawn->GetLocationOnPlanet();
-		const FVector2D NormalizeDirection = (TargetLocation - CurrentLocation)/
+		const FVector2D NormalizeDirection = (TargetLocation - CurrentLocation) /
 			FVector2D::Distance(TargetLocation, CurrentLocation);
 		const FVector2D NewLocation = CurrentLocation + NormalizeDirection * Speed * DeltaTime;
 		
 		OwnerSpherePawn->SetLocationOnPlanet(NewLocation);
-		IsMoving = FVector2D::Distance(CurrentLocation,TargetLocation) > MinimalDistance;
+		IsMoving = FVector2D::Distance(CurrentLocation, TargetLocation) > MinimalDistance;
 	}
-	
 }
 
 void USphericalMovement::SetSpeed(float NewSpeed)
@@ -45,4 +43,3 @@ void USphericalMovement::MoveTo(FVector2D Vector2D)
 	TargetLocation = Vector2D;
 	IsMoving = true;
 }
-
